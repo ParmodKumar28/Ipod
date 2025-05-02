7d840fd110b4116e0ba4ca6c404471bb55083ce3
Parmod Kumar <yparmodyadav@gmail.com>
2025-05-02 22:48:42 +0530

    Button's not working in mobile fixed

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,12 +1,11 @@
 // Imports
 import React, { Component } from "react";
-import styles from "../Styles/buttons.module.css"
+import styles from "../Styles/buttons.module.css";
 import ZingTouch from "zingtouch";
 
-
 // Class component for displaying the keypad buttons to control ipod.
-export default class Buttons extends Component{
-    constructor(props){
+export default class Buttons extends Component {
+    constructor(props) {
         super(props);
         this.rollerRef = React.createRef();
     }
@@ -15,82 +14,104 @@ export default class Buttons extends Component{
     componentDidUpdate() {
         const { menu, handleActiveMenuItemRotation } = this.props;
         const musicMenu = menu.musicMenu;
-    
+
         if (menu.menuVisible) {
-          // Accessing the roller DOM element using the ref.
-          const rollerElement = this.rollerRef.current;
-    
-          // Region where zingtouch will work.
-          const activeRegion = new ZingTouch.Region(rollerElement);
-    
-          let initialDistance = 0; // Track initial distance for rotation direction
-          let selectedIndex =
-            menu.menuType === "Menu"
-              ? menu.activeMenuItem
-              : musicMenu.activeMenuItem;
-    
-          // Binding rotate event to the roller element
-          activeRegion.bind(rollerElement, "rotate", (event) => {
-            const rotation = Math.abs(event.detail.distanceFromOrigin - initialDistance);
-            const rotationThreshold = 45; // Set your preferred threshold
-    
-            if (rotation > rotationThreshold) {
-              if (initialDistance < event.detail.distanceFromOrigin) {
-                // Clockwise rotation
-                selectedIndex =
-                  (selectedIndex + 1) %
-                  (menu.menuType === "Menu"
-                    ? menu.menuItems.length
-                    : musicMenu.menuItems.length);
-              } else {
-                // Anti-clockwise rotation
-                selectedIndex =
-                  (selectedIndex - 1 +
-                    (menu.menuType === "Menu"
-                      ? menu.menuItems.length
-                      : musicMenu.menuItems.length)) %
-                  (menu.menuType === "Menu"
-                    ? menu.menuItems.length
-                    : musicMenu.menuItems.length);
-              }
-              handleActiveMenuItemRotation(selectedIndex);
-              initialDistance = event.detail.distanceFromOrigin; // Update initial distance
-            }
-          });
+            // Accessing the roller DOM element using the ref.
+            const rollerElement = this.rollerRef.current;
+
+            // Region where zingtouch will work.
+            const activeRegion = new ZingTouch.Region(rollerElement);
+
+            let initialDistance = 0; // Track initial distance for rotation direction
+            let selectedIndex =
+                menu.menuType === "Menu"
+                    ? menu.activeMenuItem
+                    : musicMenu.activeMenuItem;
+
+            // Binding rotate event to the roller element
+            activeRegion.bind(rollerElement, "rotate", (event) => {
+                const rotation = Math.abs(event.detail.distanceFromOrigin - initialDistance);
+                const rotationThreshold = 45; // Set your preferred threshold
+
+                if (rotation > rotationThreshold) {
+                    if (initialDistance < event.detail.distanceFromOrigin) {
+                        // Clockwise rotation
+                        selectedIndex =
+                            (selectedIndex + 1) %
+                            (menu.menuType === "Menu"
+                                ? menu.menuItems.length
+                                : musicMenu.menuItems.length);
+                    } else {
+                        // Anti-clockwise rotation
+                        selectedIndex =
+                            (selectedIndex - 1 +
+                                (menu.menuType === "Menu"
+                                    ? menu.menuItems.length
+                                    : musicMenu.menuItems.length)) %
+                            (menu.menuType === "Menu"
+                                ? menu.menuItems.length
+                                : musicMenu.menuItems.length);
+                    }
+                    handleActiveMenuItemRotation(selectedIndex);
+                    initialDistance = event.detail.distanceFromOrigin; // Update initial distance
+                }
+            });
         }
-      }
+    }
 
     // Rendering Component
-    render()
-    {
+    render() {
         // Props
-        const {handleMenuBtnClick, handleOkBtnClick, handlePrevBtn, handleNextBtn, togglePlayPause, songs} = this.props;
+        const { handleMenuBtnClick, handleOkBtnClick, handlePrevBtn, handleNextBtn, togglePlayPause, songs } = this.props;
 
         // Returnning JSX
         return (
             <>
-            <div className={styles.buttonsContainer}>
-                <div ref={this.rollerRef} className={styles.roller}>
-                <div className={styles.menu} onClick={handleMenuBtnClick}>
-                    MENU
+                <div className={styles.buttonsContainer}>
+                    <div ref={this.rollerRef} className={styles.roller}>
+                        <div
+                            className={styles.menu}
+                            onClick={handleMenuBtnClick}
+                            onTouchStart={handleMenuBtnClick} // Added touch event
+                        >
+                            MENU
+                        </div>
+                        <div
+                            className={styles.next}
+                            onClick={handleNextBtn}
+                            onTouchStart={handleNextBtn} // Added touch event
+                        >
+                            <img src="https://cdn-icons-png.flaticon.com/128/4211/4211373.png" />
+                        </div>
+                        <div
+                            className={styles.play}
+                            onClick={togglePlayPause}
+                            onTouchStart={togglePlayPause} // Added touch event
+                        >
+                            <img
+                                src={
+                                    songs.isPlaying
+                                        ? "https://cdn-icons-png.flaticon.com/128/4211/4211362.png"
+                                        : "https://cdn-icons-png.flaticon.com/128/4211/4211344.png"
+                                }
+                                alt={songs.isPlaying ? "Pause Icon" : "Play Icon"}
+                            />
+                        </div>
+                        <div
+                            className={styles.prev}
+                            onClick={handlePrevBtn}
+                            onTouchStart={handlePrevBtn} // Added touch event
+                        >
+                            <img src="https://cdn-icons-png.flaticon.com/128/4211/4211379.png" />
+                        </div>
+                        <div
+                            className={styles.innerBtn}
+                            onClick={handleOkBtnClick}
+                            onTouchStart={handleOkBtnClick} // Ensure touch events are handled for mobile
+                        ></div>
+                    </div>
                 </div>
-                <div className={styles.next} onClick={handleNextBtn}>
-                <img src="https://cdn-icons-png.flaticon.com/128/4211/4211373.png"/>
-                </div>
-                <div className={styles.play} onClick={togglePlayPause}>
-                <img src={songs.isPlaying ? "https://cdn-icons-png.flaticon.com/128/4211/4211362.png"
-                 : "https://cdn-icons-png.flaticon.com/128/4211/4211344.png"}
-                alt={songs.isPlaying ? "Pause Icon" : "Play Icon"}
-                />
-                </div>
-                <div className={styles.prev} onClick={handlePrevBtn}>
-                <img src="https://cdn-icons-png.flaticon.com/128/4211/4211379.png"/>
-                </div>
-                <div className={styles.innerBtn} onClick={handleOkBtnClick}>
-                </div>
-                </div>
-            </div>
             </>
-        )
+        );
     }
 }
